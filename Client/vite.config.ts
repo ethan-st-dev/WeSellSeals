3a655041ac9bb856f225b6251a8f5b0bf787487c
ethan-st-dev <ethanstafford.dev@gmail.com>
2026-01-02 15:54:50 -0800

    ingoring coverage

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -14,5 +14,21 @@ export default defineConfig(({ mode }) => ({
     environment: "jsdom",
     setupFiles: "./app/test/setup.ts",
     css: true,
+    coverage: {
+      provider: 'v8', // or 'istanbul'
+      reporter: ['text', 'json', 'html'],
+      include: [
+        'app/components/**/*.{ts,tsx}',
+        'app/routes/**/*.{ts,tsx}',
+      ],
+      exclude: [
+        'app/test/**',
+        '**/*.test.{ts,tsx}',
+        '**/*.spec.{ts,tsx}',
+        '**/node_modules/**',
+      ],
+      all: true, // This is the key! Reports on all files, even untested ones
+    }
   },
+
 }));
